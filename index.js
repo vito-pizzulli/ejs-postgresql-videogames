ed9607cb6e43487322d1ff6a75046ab25cf93844
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-11-08 18:21:02 +0100

    added POST request to handle add of new game to the library, success & error visualized to the user via flash messages & session

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import bodyParser from "body-parser";
 import pg from "pg";
 import axios from "axios";
 import 'dotenv/config';
+import session from "express-session";
+import flash from "connect-flash";
 
 const app = express();
 const port = 3000;
@@ -19,6 +21,13 @@ db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(session({
+    secret: 'test',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+app.use(flash());
 
 app.get("/", async (req, res) => {
     try {
@@ -27,14 +36,14 @@ app.get("/", async (req, res) => {
     
         if (page && !search) {
             const result = await axios.get(`${apiUrl}&page=${page}`);
-            res.render("index.ejs", { result: result.data, currentPage: page});
+            res.render("index.ejs", { result: result.data, currentPage: page , successMessage: req.flash('success'), errorMessage: req.flash('error')});
 
         } else if (page && search) {
-            const result = await axios.get(`${apiUrl}&search=${search}&page=${page}&ordering=-rating`);
-            res.render("index.ejs", { result: result.data, currentPage: page, searchedText: search });
+            const result = await axios.get(`${apiUrl}&search=${search}&page=${page}`);
+            res.render("index.ejs", { result: result.data, currentPage: page, searchedText: search, successMessage: req.flash('success'), errorMessage: req.flash('error') });
 
         } else {
-            res.render("index.ejs");
+            res.render("index.ejs", { successMessage: req.flash('success'), errorMessage: req.flash('error')});
         }
 
         } catch (error) {
@@ -42,6 +51,31 @@ app.get("/", async (req, res) => {
             res.status(500);
         }
     });
+
+app.get("/library", (req, res) => {
+    res.render("library.ejs");
+});
+
+app.post("/add", async (req, res) => {
+    try {
+        const title = req.body.title;
+        const release_date = req.body.released;
+        const rating = req.body.rating;
+        const image = req.body.image;
+
+        const result = await db.query(
+            'INSERT INTO videogames(title, release_date, rating, image) VALUES($1, $2, $3, $4)', [title, release_date, rating, image]
+        );
+        req.flash('success', 'Game added successfully to your library!');
+        res.redirect("/");
+
+    } catch (err) {
+        console.error(err);
+        req.flash('error', 'You have already added this game to your library.');
+        res.redirect("/");
+
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
